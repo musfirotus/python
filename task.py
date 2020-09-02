294b55de28d542f0a987319b7d0989b87207558b
musfirotus <musfirotus@gmail.com>
2020-09-02 18:50:34 +0700

    Number 4

--- task.py
+++ task.py
@@ -21,7 +21,7 @@ print (sorted_dict)
 
 
 
-# Number 3
+# Number 3 => belum bisa replace sesuai jumlah
 from collections import Counter
 
 text_1 = "Mammals".lower()
@@ -32,3 +32,19 @@ def charcount(stri):
 
 print(charcount(text_1))
 print(charcount(text_2))
+
+
+
+# Number 4 => step 6 kenapa jadi step 2??
+def bubble_sort(items) :
+    for i in range(len(items)-1,0,-1):
+        for j in range(i):
+            if items[j]>items[j+1]:
+                print('Step ', j+1, ':', items)
+                temp = items[j]
+                items[j] = items[j+1]
+                items[j+1] = temp
+    
+numbers = [12,3,5,4,8,9]
+bubble_sort(numbers)
+# print(numbers)
