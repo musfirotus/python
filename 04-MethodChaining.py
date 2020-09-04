1ff2a5d32432d9a783a475d9a3a8ff9e6e4cb8f6
musfirotus <musfirotus@gmail.com>
2020-09-04 08:14:52 +0700

    Number 4

--- 04-MethodChaining.py
+++ 04-MethodChaining.py
@@ -11,10 +11,20 @@ class Cart:
         self.items.append(items)
         return self
 
-    def removeItem(self, id):
-        for i, d in enumerate(items):
-            if d['id'] == id:
-                return items.pop(i)
+    def removeItem(self,value):
+        for i, val in enumerate(self.items):
+            if val['item_id'] == value['item_id']: # konsep nya sama seperti forEach Javascript
+                del self.items[i]
+        return self
+
+    def totalItems(self):
+        count = len(self.items)
+        print(f"Total items: {count}")
+
+    # def removeItem(self, id):
+    #     for i, d in enumerate(items):
+    #         if d['id'] == id:
+    #             return items.pop(i)
 
     # def totalItems()
 
@@ -23,11 +33,14 @@ cart = Cart()
 # Do some chainings
 (
 cart.addItem({ 'item_id': 1, 'price': 30000, 'quantity': 3 })     # By default quantity is 1
+    # .addItem({ 'item_id': 2, 'price': 10000 })
     .addItem({ 'item_id': 3, 'price': 5000, 'quantity': 2 })
+    .removeItem({'item_id': 2})
     .addItem({ 'item_id': 4, 'price': 400, 'quantity': 6 })
+    # .addDiscount('50%')
 )
 
-# cart.totalItems() # 3
+cart.totalItems() # 3
 
 # cart.totalQuantity() # 11
 
