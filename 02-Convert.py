1b2a3073f9d3140ecd9b8d85c423101ae5ec8ddf
musfirotus <musfirotus@gmail.com>
2020-09-04 21:36:57 +0700

    Number 2

--- 02-Convert.py
+++ 02-Convert.py
@@ -1,8 +1,9 @@
 '''Convert number to string'''
 from num2words import num2words
+# fromnya merah karena num2words butuh package 'wheel'
 
 def Convert(num):
-    return num2words(num, lang='id')
+    return num2words(num, lang='id', to='cardinal') # sebenarnya defaultnya sudah cardinal
 
 satu = Convert(1)
 belasan = Convert(12)
@@ -10,3 +11,5 @@ puluhan = Convert(30)
 print(satu)
 print(belasan)
 print(puluhan)
+
+# Code by Musfirotus
