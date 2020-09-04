9e933776954944252e4a4d385a447eaafaf6a714
musfirotus <musfirotus@gmail.com>
2020-09-04 22:16:35 +0700

    Number 1

--- 01-Mode.py
+++ 01-Mode.py
@@ -1,1 +1,13 @@
 '''Mode to show Frequent number'''
+
+# Referensi : https://www.tutorialspoint.com/find-most-frequent-element-in-a-list-in-python
+def Mode(val):
+    result = max(set(val), key = val.count)
+    print("Element with highest frequency:\n",result)
+
+numbers = [1,2,3,4,5,6,6,8,8,6,9]
+grades = [87.5, 88.5, 60.5, 90.5, 88.5]
+
+Mode(numbers)
+
+Mode(grades)
