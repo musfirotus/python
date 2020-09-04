61c1a83366fa734832d6259d6427400d61bf077a
musfirotus <musfirotus@gmail.com>
2020-09-04 21:33:36 +0700

    Number 2

--- 02-Convert.py
+++ 02-Convert.py
@@ -1,1 +1,12 @@
 '''Convert number to string'''
+from num2words import num2words
+
+def Convert(num):
+    return num2words(num, lang='id')
+
+satu = Convert(1)
+belasan = Convert(12)
+puluhan = Convert(30)
+print(satu)
+print(belasan)
+print(puluhan)
