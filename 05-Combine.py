3208127e518a4e1b3b125a5c4511f3c9d8072139
musfirotus <musfirotus@gmail.com>
2020-09-05 00:58:57 +0700

    Number 5

--- 05-Combine.py
+++ 05-Combine.py
@@ -1,1 +1,16 @@
 '''Combine Data'''
+
+import urllib.request, json
+
+with urllib.request.urlopen("https://jsonplaceholder.typicode.com/posts") as url:
+    posts = json.loads(url.read().decode())
+
+with urllib.request.urlopen("https://jsonplaceholder.typicode.com/users") as url:
+    users = json.loads(url.read().decode())
+
+def filter_user(id):
+    return list(filter(lambda a: a['id'] == id, users))[0]
+
+for post in posts:
+    post['user'] = filter_user(post['userId'])
+    print(json.dumps(post, indent=4))
