46fac5a961f4ac4e70fe525f148bb631d2721132
musfirotus <musfirotus@gmail.com>
2020-09-04 21:43:37 +0700

    Number 3

--- 03-Trim.py
+++ 03-Trim.py
@@ -1,1 +1,7 @@
 '''Trim characters/text'''
+
+def Trim(val = "", lim = 6, seperator = "..."):
+    info = (val[:lim] + '..') if lim >= 2 else "Minimal limit 2"
+    print(info)
+
+Trim("ini adalah tulisan yang sangat panjang",7)
