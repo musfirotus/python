aaae7ab89b11cbebb080ac6a7ce5eb73eb4eba67
musfirotus <musfirotus@gmail.com>
2020-09-05 08:28:16 +0700

    finished

--- 05-Combine.py
+++ 05-Combine.py
@@ -10,11 +10,14 @@ with urllib.request.urlopen("https://jsonplaceholder.typicode.com/posts") as url
 with urllib.request.urlopen("https://jsonplaceholder.typicode.com/users") as url:
     users = json.loads(url.read().decode())
 
+# filter data 'users' dengan id yang sama dengan id dari parameter
 def user(id):
     return list(filter(lambda a: a['id'] == id, users))[0]
 
 for post in posts:
+    # filter data posts yang idnya sesuai dengan data users
     post['user'] = user(post['userId'])
+    # dirapikan tampilan saat dieksekusi
     print(json.dumps(post, indent=4))
 
 # Code by Musfirotus
